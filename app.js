687909dffcea7c6f304032efbe294f6f4addb8fb
Karan <kairakaran362@gmail.com>
2021-02-19 18:28:06 +0530

    Added Atlas server,now merging

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,13 +17,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// // connect to cloud  database
+mongoose.connect('mongodb+srv://admin-karan:test123@cluster0.qpybj.mongodb.net/blogWebsite',{
+  useNewUrlParser: true,   
+  useUnifiedTopology: true
+});
+
+// connect to local database
+
+// mongoose.connect('mongodb://localhost:27017/blogDB',{
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+const postSchema = new mongoose.Schema({
+  title : String,
+  content :String
+});
+
+const Post = mongoose.model('Post',postSchema);
+
+
 
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+ 
+  Post.find({},(err,posts)=>{
+    res.render('home',{
+      startingContent: homeStartingContent,
+      posts:posts
     });
+  });
+
 });
 
 app.get("/about", function(req, res){
@@ -37,31 +63,24 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+  const newPost = new Post ({
+    title : req.body.postTitle,
+    content : req.body.postBody
+  });
+  
+  newPost.save(()=>{
+    res.redirect("/");
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:post_id", function(req, res){
+  const requestedPost = req.params.post_id;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+  Post.findById( requestedPost,(err,foundPost)=>{
+    res.render('post',{thispost : foundPost});
   });
-
 });
 
 app.listen(3000, function() {
