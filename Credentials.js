97e8e8eb648f34a6bb8a25a0f16758b53371aa47
Karan <kairakaran362@gmail.com>
2021-02-22 18:06:09 +0530

    Restructing Repo

--- Credentials.js
+++
@@ -1,9 +0,0 @@
-//  I creating new file for credentials so that  I can push this file to git without exposing my credentials
-
-
-const Username = 'admin-karan';
-const Password = 'test123'
-
-exports.Username = Username;
-exports.Password = Password;
-
